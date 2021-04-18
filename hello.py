b74e0e8db9c18269916a92d915017f74b2afb7e2
ShuheiT <amaleg77@gmail.com>
2021-04-18 15:44:42 +0900

    add fizzBuzz

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+def fizz_buzz():
+    result = ("FizzBuzz" if i % 15 == 0 else "fizz" if i %
+              3 == 0 else "Buzz" if i % 5 == 0 else i for i in range(100))
+    for i in result:
+        print(i)
+
 
 def main():
-    a = [int(i) for i in range(1000)]
+    a = [int(i) for i in range(100)]
     even = filter(lambda x: x % 2 == 0, a)
     for i in even:
         print(i)
@@ -8,3 +14,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    fizz_buzz()
