01438f1c4a61c7e81a15ad5d55a1d476675a5d6e
ShuheiT <amaleg77@gmail.com>
2021-04-26 13:06:31 +0900

    add fizzbuzz func

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-def main():
-    a = [int(i) for i in range(1000)]
-    even = filter(lambda x: x%2 == 0, a)
-    
+def fizz_buzz():
+    result = ("FizzBuzz" if i % 15 == 0 else "fizz" if i %
+              3 == 0 else "Buzz" if i % 5 == 0 else i for i in range(100))
+    for i in result:
+        print(i)
+
 
 def even():
     a = [int(i) for i in range(100)]
